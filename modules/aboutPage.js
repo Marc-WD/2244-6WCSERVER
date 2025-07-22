abedc5c9f31573557ba39a8326c2198a14e639fc
Marc-WD <m4canlas@gmail.com>
2025-07-22 22:57:58 +0800

    Update and rename about_module.js to aboutPage.js

---
+++ modules/aboutPage.js
@@ -0,0 +1,5 @@
+function aboutPage(name) {
+    return `<h1>This is the About Page</h1>
+            <p>Hello ${name}. This activity will teach on how to deal with a simple server and local modules in Node.js</p>`;
+}
+module.exports = aboutPage;
