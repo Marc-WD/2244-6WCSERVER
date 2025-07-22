a8e036ac743f5749ee2a7ce09613306b0685ba04
Marc-WD <m4canlas@gmail.com>
2025-07-22 22:55:32 +0800

    Update and rename gallery_module.js to app.js

---
+++ modules/app.js
@@ -0,0 +1,34 @@
+// Author: John Smith
+// Date: July 22, 2025
+// Section: BSCS 3A
+
+const http = require('http');
+const rootPage = require('./rootPage');
+const aboutPage = require('./aboutPage');
+const contactPage = require('./contactPage');
+
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    if (req.url === '/') {
+        res.write(rootPage('John Smith'));
+    } else if (req.url === '/about') {
+        res.write(aboutPage('John Smith'));
+    } else if (req.url === '/contact') {
+        res.write(contactPage('John Smith'));
+    } else if (req.url === '/gallery') {
+        res.write('<h1>This is the Gallery Page</h1>');
+    } else {
+        res.write('Invalid Request');
+    }
+
+    // Footer: name, date, and section
+    res.write('<br><br><small>John Smith - July 22, 2025 - BSCS 3A</small>');
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
--- modules/gallery_module.js
+++
@@ -1,5 +0,0 @@
-function gallery(name) {
-    return `<h1> This is the Gallery Page</h1><p>Welcome ${name}!</p>`;
-}
-
-module.exports = gallery;
