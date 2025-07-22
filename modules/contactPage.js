e6d13fc266cc58cee5ea327632843f17aab1beee
Marc-WD <m4canlas@gmail.com>
2025-07-22 22:55:56 +0800

    Update and rename contact_module.js to contactPage.js

---
+++ modules/contactPage.js
@@ -0,0 +1,7 @@
+function contactPage(name) {
+    return `<h1>This is the Contact Page</h1>
+            <p>${name}, if you want additional details about this activity go to this site: 
+            <a href="https://www.tutorialrepublic.com/node-js/nodejs-nodejs-modules.php" target="_blank">
+            https://www.tutorialrepublic.com/node-js/nodejs-nodejs-modules.php</a></p>`;
+}
+module.exports = contactPage;
