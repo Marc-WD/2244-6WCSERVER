275d79847650e1b073170c763d7b5d8a5565a730
Marc-WD <m4canlas@gmail.com>
2025-07-23 14:11:14 +0800

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,26 +1,31 @@
 // Server/server.js
-const express = require('express');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 const Person = require('../Person/person');
-const { log } = require('../Logs/logs');
-
-const app = express();
-const PORT = 3000;
+const { log } = require('../Logs/logger');
 
 const dataPath = path.join(__dirname, '../Data/data.json');
 const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
-app.get('/people', (req, res) => {
-  log('GET /people');
-  const people = data.map(p => {
-    const person = new Person(p.ID, p["First Name"], p["Last Name"], p.age);
-    return person.greet();
-  });
-  res.json(people);
+const server = http.createServer((req, res) => {
+  log(`Request: ${req.method} ${req.url}`);
+
+  if (req.url === '/people') {
+    const people = data.map(p => {
+      const person = new Person(p.ID, p["First Name"], p["Last Name"], p.age);
+      return person.greet();
+    });
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(people));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Route not found');
+  }
 });
 
-app.listen(PORT, () => {
-  log(`Server running at http://localhost:${PORT}`);
+server.listen(3000, () => {
+  log('Server running on http://localhost:3000');
 });
