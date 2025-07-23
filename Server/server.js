80e337763aaec8ca1b2db339a6e632e0c89455c4
Marc-WD <m4canlas@gmail.com>
2025-07-23 14:18:16 +0800

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,28 +1,26 @@
-// Server/server.js
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
 const Person = require('../Person/person');
-const { log } = require('../Logs/logger');
+const { log } = require('../Logs/logs');
 
 const dataPath = path.join(__dirname, '../Data/data.json');
 const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
 const server = http.createServer((req, res) => {
-  log(`Request: ${req.method} ${req.url}`);
+  log(`Request received: ${req.method} ${req.url}`);
 
   if (req.url === '/people') {
     const people = data.map(p => {
-      const person = new Person(p.ID, p["First Name"], p["Last Name"], p.age);
+      const person = new Person(p.name, p.age);
       return person.greet();
     });
-
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(people));
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Route not found');
+    res.end('Not found');
   }
 });
 
