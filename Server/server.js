71b63be9fb5a6ad5e78f3af39fc01afba56d02f0
Marc-WD <m4canlas@gmail.com>
2025-07-23 14:06:46 +0800

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,26 @@
 // Server/server.js
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const Person = require('../Person/person');
 const { log } = require('../Logs/logs');
-const data = require('../Data/data');
 
 const app = express();
 const PORT = 3000;
 
-app.get('/people', (req, res) => {
-  log(`GET /people`);
+const dataPath = path.join(__dirname, '../Data/data.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
+app.get('/people', (req, res) => {
+  log('GET /people');
   const people = data.map(p => {
     const person = new Person(p.ID, p["First Name"], p["Last Name"], p.age);
     return person.greet();
   });
-
   res.json(people);
 });
 
 app.listen(PORT, () => {
-  log(`Server is running at http://localhost:${PORT}`);
+  log(`Server running at http://localhost:${PORT}`);
 });
-
