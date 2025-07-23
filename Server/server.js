e391ca59832492c907bb92b560dbf6b1024ee7c1
Marc-WD <m4canlas@gmail.com>
2025-07-23 12:48:49 +0800

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const Person = require('../Person/person');
+const { log } = require('../Logs/logs');
+
+const dataPath = path.join(__dirname, '../Data/data.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+const server = http.createServer((req, res) => {
+  log(`Request: ${req.method} ${req.url}`);
+
+  if (req.url === '/people') {
+    const people = data.map(p => {
+      const person = new Person(p.name, p.age);
+      return person.greet();
+    });
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(people));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Route not found');
+  }
+});
+
+server.listen(3000, () => {
+  log('Server running on http://localhost:3000');
+});
